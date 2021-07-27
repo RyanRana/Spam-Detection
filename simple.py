41678529b8c16c02cf34a52c66b574500ed1e29b
RyanRana <39924576+RyanRana@users.noreply.github.com>
2021-07-26 22:03:09 -0400

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+
+data = pd.read_csv("spam.csv")
+data = data[["class", "sms"]]
+
+x = np.array(data["sms"])
+y = np.array(data["class"])
+cv = CountVectorizer()
+X = cv.fit_transform(x) 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+clf = MultinomialNB()
+clf.fit(X_train,y_train)
+
+sample = input('Enter a message:')
+data = cv.transform([sample]).toarray()
+print(clf.predict(data))
